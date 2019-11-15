27ee61026eacd8eb57690ded72b6f6d697638c27
Jef Malfliet <jef.malfliet@student.hogent.be>
2019-11-15 18:07:34 +0100

    [FEAT] add models

---
+++ src/modules/activity/model.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import { Person } from '../person/model';
+
+export interface Activity{
+    _id: any,
+    name: string,
+    startDate: Date,
+    endDate: Date,
+    place: string,
+    participants: Map<Person, Role>,
+    description: string
+}
+
+export interface ActivityDocument extends Activity, Document {}
+
+const schema = new Schema(
+    {
+      name: { type: String, required: true },
+      startDate: { type: Date, required: true },
+      endDate: { type: Date, required: true },
+      place: {type: String, required: true},
+      participants: {type: Map, of: String, ref:'roles', required: false},
+      description: { type: String, required: false },
+    },
+    { _id: true, timestamps: true }
+  );
+  
+  export const model = mongoose.model<ActivityDocument>('activities', schema);
+  
---
+++ src/modules/person/model.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface Person {
+  _id: any;
+  name: string;
+  friends: Person[];
+}
+
+export interface PersonDocument extends Person, Document {}
+
+const schema = new Schema(
+  {
+    name: { type: String, required: true },
+    friends: [{ type: String, ref: "people", required: false }]
+  },
+  { _id: true, timestamps: true }
+);
+
+export const model = mongoose.model<PersonDocument>("people", schema);
